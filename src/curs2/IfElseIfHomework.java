dfff3713b227a572a08021a33f62a89f66fa250f
Andreea Birna <andreea.birna@yahoo.com>
2022-02-26 13:16:51 +0200

    homework2

---
+++ src/curs2/IfElseIfHomework.java
@@ -0,0 +1,29 @@
+package curs2;
+
+import java.util.Scanner;
+
+public class IfElseIfHomework {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ce varsta ai? ");
+		
+		Scanner scan = new Scanner(System.in);
+		int varsta = scan.nextInt();
+		
+				
+		if(varsta < 18) {
+			System.out.println("Esti minor. ");
+		
+		
+		}else if(varsta >= 18 && varsta <= 65) {
+			System.out.println("Esti adult. ");
+		
+		
+		}else
+			System.out.println("Esti batran. ");						
+				
+
+	}
+
+}
